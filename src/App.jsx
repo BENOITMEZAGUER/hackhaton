3d3cfc14a386d7a438e651ae15da2d099d31620e
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-07-03 15:03:27 +0200

    first commut

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Survey from "./components/Survey/Survey";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <>
+      <Survey />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Survey/Survey.jsx
@@ -0,0 +1,61 @@
+function Survey() {
+  return (
+    <div>
+      <form data-dashlane-rid="a886061a28874446" data-form-type="other">
+        <fieldset>
+          <legend className="mt-4">Question 1</legend>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+              data-dashlane-rid="1bc05ab84ff89c68"
+              data-form-type="other"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Default checkbox
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend className="mt-4">Question 2</legend>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+              data-dashlane-rid="1bc05ab84ff89c68"
+              data-form-type="other"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Default checkbox
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend className="mt-4">Question 3</legend>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+              data-dashlane-rid="1bc05ab84ff89c68"
+              data-form-type="other"
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Default checkbox
+            </label>
+          </div>
+        </fieldset>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Survey;
