aeee56e29351442cfe66833e76d9cd759befd6be
Mathieu DELFOSSE <mat.delfosse@gmail.com>
2024-07-04 18:05:20 +0200

    Page droits du travail

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
